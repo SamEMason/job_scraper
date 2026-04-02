64d82b950b2fa02b690698430a44afbbda858642
sam <me@sammason.dev>
2026-04-02 16:54:19 -0400

    implement feature flags for tasks, move main call into index.ts, update npm script

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ScrapeJobs } from '#src/tasks/ScrapeJobs.ts';
+import Config from '#src/Config.ts';
 
-const main = async () => {
-  const scrapeJobTask = new ScrapeJobs();
-  await scrapeJobTask.run();
+export default async function main() {
+  if (Config.SCRAPE_JOBS_ENABLED) {
+    const scrapeJobTask = new ScrapeJobs();
+    await scrapeJobTask.run();
+    const jobs = scrapeJobTask.getJobs();
+    console.log(jobs);
+  }
 
-  const jobs = scrapeJobTask.getJobs();
-  console.log(jobs);
-};
+  if (Config.DEPT_UID_DISCOVERY_ENABLED) {
+    console.log('Department UID discovery initiated!');
+  }
 
-main();
+  return 0;
+}
